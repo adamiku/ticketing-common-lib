2a8cae75ab70adf3161229dc784a8cd94bf5987f
Adam <adam_mikulas@mckinsey.com>
2024-02-21 11:54:24 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// reexported stuff from errors and middlewares
+
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
